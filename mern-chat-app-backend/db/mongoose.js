ceba7e1a8e5bba3b6de67942c4246224710c0ecb
Parshvavyas17 <parshvavyas07@gmail.com>
2023-02-14 22:21:05 +0530

    Env changes

--- mern-chat-app-backend/db/mongoose.js
+++ mern-chat-app-backend/db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
 mongoose.connect(
-  `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@chat-app-database.7yafmjv.mongodb.net/?retryWrites=true&w=majority`,
+  `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_LINK}/chat-app?retryWrites=true&w=majority`,
   () => {
     console.log("Mongo DB connected successfully.");
   }
