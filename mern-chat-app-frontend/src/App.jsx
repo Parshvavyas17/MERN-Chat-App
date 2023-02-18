c467bcec88de3f48812186d81084eda5cb4f29b7
Parshvavyas17 <parshvavyas07@gmail.com>
2023-02-19 00:40:38 +0530

    Routes protected

--- mern-chat-app-frontend/src/App.jsx
+++ mern-chat-app-frontend/src/App.jsx
@@ -5,16 +5,23 @@ import Home from "./pages/Home/Home";
 import Chat from "./pages/Chat/Chat";
 import Login from "./pages/Authentication/Login";
 import Signup from "./pages/Authentication/Signup";
+import { useSelector } from "react-redux";
 
 function App() {
+  const user = useSelector((state) => state.user);
+
   return (
     <BrowserRouter>
       <NavigationBar />
       <Routes>
         <Route path="/" element={<Home />} />
+        {!user && (
+          <>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </>
+        )}
         <Route path="/chat" element={<Chat />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
